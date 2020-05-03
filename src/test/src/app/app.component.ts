1ef88bfcb7f9db8cf2e2933b05e08e3d905e46ad
VA-PC\jan <jan@vi-a.de>
2020-05-03 19:08:30 +0200

    update

--- src/test/src/app/app.component.ts
+++ src/test/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
+    selector: 'lasthub-test',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     encapsulation: ViewEncapsulation.ShadowDom,
