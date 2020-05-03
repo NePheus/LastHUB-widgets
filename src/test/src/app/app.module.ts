a5e5827decf7905fc226d8ed581b67e56d7286eb
VA-PC\jan <jan@vi-a.de>
2020-05-03 20:01:55 +0200

    update

--- src/test/src/app/app.module.ts
+++ src/test/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Injector } from '@angular/core';
+import { NgModule, Injector, DoBootstrap } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { TestComponent } from './test/test.component';
 
@@ -8,7 +8,7 @@ import { TestComponent } from './test/test.component';
     imports: [BrowserModule],
     entryComponents: [TestComponent],
 })
-export class AppModule {
+export class AppModule implements DoBootstrap {
     constructor(private _injector: Injector) {
         const el = createCustomElement(TestComponent, { injector: this._injector });
         customElements.define('hub-widget-test', el);
